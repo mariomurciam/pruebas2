29beb31847ca7e8d8f70623a5319ecac241745e7
mariomurciam <mariomurciam@gmail.com>
2023-12-15 15:13:22 +0100

    PruebaNet

---
+++ PruebaNet/Assets/Script/Btn1.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Btn1 : MonoBehaviour
+{
+    public TMP_Text text;
+    
+    public void Uno()
+    {
+        text.text = "1";
+    }
+
+    
+}
