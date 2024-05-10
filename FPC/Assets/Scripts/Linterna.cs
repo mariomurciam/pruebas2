3c1e9ed2b3c5b413018f0884467d3e012f109d04
mariomurciam <mariomurciam@gmail.com>
2024-05-10 06:24:41 +0200

    update

--- FPC/Assets/Scripts/Linterna.cs
+++ FPC/Assets/Scripts/Linterna.cs
@@ -5,17 +5,6 @@ using UnityEngine;
 public class Linterna : MonoBehaviour
 {
     public GameObject linterna;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-    private void FixedUpdate()
-    {
-        linterna.transform.position = transform.position;
-        linterna.transform.rotation = transform.rotation;
-    }
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
